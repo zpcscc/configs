989f4ed5b741dcbe7627d06134988bc9f4ec0adc
dxsixpc <1071667708@qq.com>
2023-02-27 23:26:21 +0800

    🔥update: 删除无用的types类型

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,4 @@
 /// <reference types="node" />
 /// <reference types="react" />
 /// <reference types="react-dom" />
-/// <reference types="./custom" />
 /// <reference types="./files" />
-/// <reference types="./utils" />
