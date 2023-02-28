277ada3c19df8825825dc21fa384619222a124da
dxsixpc <1071667708@qq.com>
2023-03-01 01:30:46 +0800

    🔥update: 更新eslint与prettier,新增文档，优化配置

--- src/eslint-config/index.js
+++ src/eslint-config/index.js
@@ -5,7 +5,7 @@
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  root: true,
+  // 使用typescript解析器
   parser: '@typescript-eslint/parser',
   parserOptions: {
     // 使用额外的语言特性
@@ -16,6 +16,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  // 运行的环境
   env: {
     browser: true,
     commonjs: true,
@@ -27,85 +28,88 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'prettier',
+    // ts推荐配置，做了一些兼容处理
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+    // eslint-plugin-import---对import的导入进行检查
+    'plugin:import/recommended',
+    // eslint-import-resolver-typescript---import校验对typescript的支持。是eslint-plugin-import插件的补充
+    'plugin:import/typescript',
+    // 对jsx的支持与推荐配置
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
+    // 对promise的推荐配置
     'plugin:promise/recommended',
+    // 对react的推荐配置
     'plugin:react/recommended',
+    // react17及以上，新的jsx转换规则，需要引入react/jsx-runtime以禁止部分规则
     'plugin:react/jsx-runtime',
+    // react-hook的推荐配置
     'plugin:react-hooks/recommended',
+    // sonarjs的推荐配置
     'plugin:sonarjs/recommended',
+    // ts的标准支持
     'standard-with-typescript',
+    // eslint-plugin-prettier的推荐配置，需要放在最后一位，内部启用了eslint-plugin-prettier插件
+    // 用于关闭eslint中的所有格式化配置，全部在.prettierrc中进行格式化配置
+    'plugin:prettier/recommended',
   ],
+  // 如果要在rules中针对某个插件做具体配置，则需要在plugins里先引入对应插件
   plugins: [
     '@typescript-eslint',
-    'eslint-comments',
-    'import',
-    'prettier',
-    'promise',
     'react',
     'react-hooks',
     'sonarjs',
     'unicorn',
+    'promise',
+    'import',
   ],
   // eslint-plugin-react的配置https://github.com/jsx-eslint/eslint-plugin-react#configuration
   settings: {
     react: {
       version: 'detect',
     },
+    // eslint-import-resolver-typescript
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
   },
   rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/lines-between-class-members': 'off',
+    // ts不允许有any类型。关闭此校验，特殊情况，需要any类型
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-empty-interface': 'warn',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-redeclare': [2, { ignoreDeclarationMerge: true }],
-    '@typescript-eslint/no-shadow': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-useless-constructor': 'off',
+    // 显示函数返回类型。关闭此校验，部分函数没有返回值。无需每个都显示类型
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // 严格的逻辑表达式。关闭此选项，可以使用 !value 的方式来进行判断；
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    // 为了安全使用 ?? 替换 ||。关闭此选项，部分场景，需要手动选择??还是||； ??会把空字符串也认为是true
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    // 无混淆空表达式。默认所有void都不能直接返回，需要用{}包裹。这里排除掉箭头函数。
+    // 可以直接写成 onchange={(value)=> onChange(value) }
+    // 而不是 onchange={(value)=>{ onChange(value) }}
+    '@typescript-eslint/no-confusing-void-expression': ['warn', { ignoreArrowShorthand: true }],
+    // import 类型时，需要加 type
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    // 禁止变量重新声明
+    '@typescript-eslint/no-redeclare': 'warn',
+    // 禁止未使用的表达式
+    'no-unused-expressions': 'off',
     '@typescript-eslint/no-unused-expressions': [
       2,
       { allowShortCircuit: true, allowTernary: true },
     ],
-    '@typescript-eslint/no-unused-vars': [
-      2,
-      {
-        vars: 'all',
-        args: 'after-used',
-        ignoreRestSiblings: true,
-        varsIgnorePattern: 'React',
-      },
-    ],
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/semi': 'off',
+
+    // 强制填写了默认值的参数在最后。关闭此选项，部分函数参数值，无法任意调整位置。
+    'default-param-last': 'off',
+    // props类型检查.关闭此校验，ts有静态类型检查
+    'react/prop-types': 'off',
+    // react17之后引入新的jsx转换。不再需要显示引入react。这两项规则需要关闭。
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+
+    // 以下配置待整理完善，以上配置已整理，尽量不要动了。
     camelcase: 'off',
-    // 是否尾随逗号
-    'comma-dangle': [
-      'error',
-      {
-        // 数组，同一行时不需要，不同行时需要
-        arrays: 'always-multiline',
-        exports: 'never',
-        // 函数值，最后不需要尾随逗号
-        functions: 'never',
-        imports: 'never',
-        // 对象，同一行时不需要，不同行时需要
-        objects: 'always-multiline',
-      },
-    ],
     'consistent-return': 'off',
     'class-methods-use-this': 'off',
     'default-case-last': 'error',
-    'default-param-last': 'error',
     eqeqeq: [2, 'always', { null: 'ignore' }],
     'func-names': [1, 'never', { generators: 'as-needed' }],
     'func-style': 'error',
@@ -117,10 +121,8 @@ module.exports = {
     'import/no-relative-packages': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
-    'implicit-arrow-linebreak': 'off',
     'lines-between-class-members': 'off',
     'max-classes-per-file': 'off',
-    'multiline-comment-style': 'error',
     'no-bitwise': 'error',
     'no-cond-assign': [2, 'except-parens'],
     'no-constructor-return': 'error',
@@ -128,7 +130,6 @@ module.exports = {
     'no-continue': 'error',
     'no-else-return': 'error',
     'no-eval': 'error',
-    'no-implied-eval': 'error',
     'no-implicit-coercion': 'error',
     'no-inner-declarations': 'off',
     'no-labels': 'error',
@@ -137,7 +138,6 @@ module.exports = {
     'no-promise-executor-return': 'error',
     'no-param-reassign': [2, { props: false }],
     'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
-    'no-redeclare': 'off',
     'no-return-assign': [2, 'except-parens'],
     'no-return-await': 'error',
     'no-shadow': 'off',
@@ -145,8 +145,6 @@ module.exports = {
     'no-nested-ternary': 'off',
     'no-unreachable-loop': 'error',
     'no-unneeded-ternary': 'error',
-    'no-unused-expressions': 'error',
-    'no-unused-vars': 'off',
     'no-use-before-define': 'off',
     'no-useless-computed-key': 'error',
     'no-useless-concat': 'error',
@@ -158,20 +156,13 @@ module.exports = {
     'prefer-template': 'error',
     'promise/always-return': 'off',
     'promise/catch-or-return': 'off',
-    'prettier/prettier': 'off',
     quotes: 'error',
     'react/destructuring-assignment': 'off',
     'react/jsx-props-no-spreading': 'off',
-    'react/jsx-uses-react': 'off',
     'react/no-access-state-in-setstate': 'off',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
     'react/self-closing-comp': 'warn',
     'react/sort-comp': 'off',
     'require-atomic-updates': 'error',
-    // 总是分号结尾
-    semi: ['error', 'always'],
-    'sort-imports': 'off',
     'sonarjs/cognitive-complexity': 'off',
     'sonarjs/no-nested-template-literals': 'off',
     'sonarjs/no-duplicate-string': 'off',
--- src/prettier-config/index.js
+++ src/prettier-config/index.js
@@ -1,22 +1,39 @@
 // 官方文档 https://prettier.io/
 module.exports = {
+  // 额外的插件
   plugins: [
+    // import格式化时自动排序
     require.resolve('prettier-plugin-organize-imports'),
+    // package.json格式化时自动排序
     require.resolve('prettier-plugin-packagejson'),
   ],
+  // import格式化自动排序时，不删除未使用的import
   organizeImportsSkipDestructiveCodeActions: true,
+  // 结尾需要分号
   semi: true,
+  // 使用单引号
   singleQuote: true,
+  // 每行最大长度
   printWidth: 100,
+  // 结尾需要逗号
   trailingComma: 'all',
+  // 每个缩进级别的空格数
   tabWidth: 2,
+  // 是否使用制表符缩进
   useTabs: false,
+  // 是否换行
   proseWrap: 'never',
-  endOfLine: 'auto',
+  // 换行符，linux和macos中为'lf'，window中为'crlf'
+  endOfLine: 'lf',
+  // 在jsx中是否使用单引号
   jsxSingleQuote: true,
+  // 对象和括号之间是否需要空格
   bracketSpacing: true,
-  jsxBracketSameLine: false,
+  // 将多行html标签放在最后一行末尾。而不是重开一行。这里关闭，希望重开一行。
+  bracketSameLine: false,
+  // 箭头函数是否需要括号。
   arrowParens: 'always',
+  // 重写部分文件的规则
   overrides: [
     { files: '.eslintrc', options: { parser: 'json' } },
     { files: '.prettierrc', options: { parser: 'json' } },
