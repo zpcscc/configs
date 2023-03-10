46cc9f6e0a859cf4e74571c16cd527933db1f319
dxsixpc <1071667708@qq.com>
2023-03-10 15:47:43 +0800

    ✨feat: 更新eslint配置，配置自动发布文档

--- src/eslint-config/index.js
+++ src/eslint-config/index.js
@@ -90,6 +90,12 @@ module.exports = {
     '@typescript-eslint/consistent-type-imports': 'warn',
     // 禁止变量重新声明
     '@typescript-eslint/no-redeclare': 'warn',
+    // 无浮动的promise，要求处理每个promise的then和catch。
+    '@typescript-eslint/no-floating-promises': 'off',
+    // 强制使用一致性类型断言。<>语法，或者as语法
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    // 禁止未知属性。这里忽略emotion里的css属性报错。将css属性视为正常属性
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
     // 禁止未使用的表达式
     'no-unused-expressions': 'off',
     '@typescript-eslint/no-unused-expressions': [
